8aec5011c18f6f801a8432296f816bf4deb79490
NoelCarlton <tonight.26@hotmail.com>
2020-06-05 17:34:55 +0800

    接口獲取後臺數據並顯示

--- admin/src/views/teacher/index.vue
+++ admin/src/views/teacher/index.vue
@@ -1,28 +1,50 @@
 <template>
-  <div class="app-container">
-    <h1>教师查询</h1>
-  </div>
+  <el-table :data="list"
+            style="width: 100%">
+    <el-table-column prop="id"
+                     label="編號"
+                     width="180">
+    </el-table-column>
+    <el-table-column prop="name"
+                     label="姓名"
+                     width="180">
+    </el-table-column>
+    <el-table-column prop="intro"
+                     label="描述">
+    </el-table-column>
+    <el-table-column prop="career"
+                     label="職位">
+    </el-table-column>
+    <el-table-column prop="gmtCreate"
+                     label="入駐時間">
+    </el-table-column>
+  </el-table>
 </template>
 <script>
-import teacher from "@/api/teacher/teacher"
+import teacher from '@/api/teacher/teacher'
 export default {
-    data() {
-        return {
-            current:1,
-            limit:10,
-            total:0,
-            query:{},
-            list:null
-        }
-    },
-    created(){
-
-    },
-    methods: {
-        getTeachers() { 
-            teacher.getTeachers(this.current, this.limit, this.query)
-            .then(response =>{console.log(response)})
-            .catch(err =>{console.log(err)})
-        }
+  data () { // 定义变量初始值
+    return {
+      current: 1,
+      limit: 10,
+      total: 0,
+      query: {},
+      list: null
     }
+  },
+  created () {
+    this.getTeachers()
+  },
+  methods: {
+    getTeachers () {
+      teacher.getList(this.current, this.limit, this.query)
+        .then(response => {
+          console.log(response) 
+          this.list = response.data.teachers
+          this.total = response.data.total
+        })
+        .catch(err => { console.log(err) })
+    }
+  }
+}
 </script>
