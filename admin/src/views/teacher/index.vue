ceb9284022e8995d1764406a6ebfce30580024ff
NoelTung <noelcarlton@foxmail.com>
2020-06-06 19:25:24 +0800

    老师分页展示、条件查询

--- admin/src/views/teacher/index.vue
+++ admin/src/views/teacher/index.vue
@@ -1,5 +1,31 @@
 <template>
-  <el-table :data="list"
+  <div class="app-container">
+    <el-form :inline="true" class="demo-form-inline">
+      <el-form-item>
+        <el-input v-model="queryInfo.name" placeholder="讲师名"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-select v-model="queryInfo.level"   clearable placeholder="讲师头衔">
+          <el-option  :value="1" label="高级讲师"></el-option>
+          <el-option  :value="2" label="首席讲师"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="添加时间">
+        <el-date-picker v-model="queryInfo.begin"
+          type="datetime"
+          placeholder="选择开始时间" 
+          value-format="yyyy-MM-dd HH:mm:ss"
+          default-time="00:00:00"></el-date-picker>
+          <el-date-picker v-model="queryInfo.end"
+          type="datetime"
+          placeholder="选择截止时间" 
+          value-format="yyyy-MM-dd HH:mm:ss"
+          default-time="00:00:00"></el-date-picker>
+      </el-form-item>
+      <el-button type="primary" icon="el-icon-search" @click="getTeachers()">查询</el-button>
+      <el-button type="default"  @click="resetDate()">清空</el-button>
+    </el-form>
+    <el-table :data="list"
             style="width: 100%">
     <el-table-column prop="id"
                      label="編號"
@@ -15,10 +41,26 @@
     <el-table-column prop="career"
                      label="職位">
     </el-table-column>
+    <el-table-column label="头衔">
+      <template slot-scope="scope">
+        {{ scope.row.level === 1 ? "高级讲师":"首席讲师"}}
+      </template>
+    </el-table-column>
     <el-table-column prop="gmtCreate"
                      label="入駐時間">
     </el-table-column>
   </el-table>
+   <div class="block">
+    <el-pagination
+      @current-change="getTeachers"
+      :current-page="current"
+      :page-size="limit"
+      layout="total, prev, pager, next, jumper"
+      :total="total"
+      style="padding:30px 0; text-align:center;">
+    </el-pagination>
+  </div>
+  </div>
 </template>
 <script>
 import teacher from '@/api/teacher/teacher'
@@ -28,7 +70,7 @@ export default {
       current: 1,
       limit: 10,
       total: 0,
-      query: {},
+      queryInfo: {},
       list: null
     }
   },
@@ -36,14 +78,19 @@ export default {
     this.getTeachers()
   },
   methods: {
-    getTeachers () {
-      teacher.getList(this.current, this.limit, this.query)
+    getTeachers (current=1) {
+      this.current = current
+      teacher.getList(this.current, this.limit, this.queryInfo)
         .then(response => {
           console.log(response) 
           this.list = response.data.teachers
           this.total = response.data.total
         })
         .catch(err => { console.log(err) })
+    },
+    resetDate(){
+      this.queryInfo ={}
+      this.getTeachers()
     }
   }
 }
